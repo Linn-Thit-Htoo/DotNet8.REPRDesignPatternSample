ecf4c4d7f5189181c58acb4592678474a5a986a8
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-24 17:18:00 +0630

    Format Code

--- DotNet8.REPRDesignPatternSample.DbService/AppDbContexts/AppDbContext.cs
+++ DotNet8.REPRDesignPatternSample.DbService/AppDbContexts/AppDbContext.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNet8.REPRDesignPatternSample.DbService.AppDbContexts;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
 
     public DbSet<Tbl_Blog> Blogs { get; set; }
 }
