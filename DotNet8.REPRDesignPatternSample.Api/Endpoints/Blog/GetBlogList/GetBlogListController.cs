83a4486f7465462c81118ad58f40ff2ed75ed318
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-24 17:17:05 +0630

    Format Code

--- DotNet8.REPRDesignPatternSample.Api/Endpoints/Blog/GetBlogList/GetBlogListController.cs
+++ DotNet8.REPRDesignPatternSample.Api/Endpoints/Blog/GetBlogList/GetBlogListController.cs
@@ -5,29 +5,28 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.REPRDesignPatternSample.Api.Endpoints.Blog.GetBlogList
+namespace DotNet8.REPRDesignPatternSample.Api.Endpoints.Blog.GetBlogList;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GetBlogListController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class GetBlogListController : ControllerBase
+    private readonly AppDbContext _context;
+
+    public GetBlogListController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public GetBlogListController(AppDbContext context)
-        {
-            _context = context;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        var dataLst = await _context.Blogs
+            .OrderByDescending(x => x.BlogId)
+            .ToListAsync();
+        var lst = dataLst.Select(x => x.Change()).ToList();
+        var model = new GetBlogListResponse { DataLst = lst };
 
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs()
-        {
-            var dataLst = await _context.Blogs
-                .OrderByDescending(x => x.BlogId)
-                .ToListAsync();
-            var lst = dataLst.Select(x => x.Change()).ToList();
-            var model = new GetBlogListResponse { DataLst = lst };
-
-            return Ok(model);
-        }
+        return Ok(model);
     }
 }
