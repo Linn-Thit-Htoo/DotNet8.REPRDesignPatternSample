6a7299e54dcab012bf51d9ef996bbdbefbc51242
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-24 17:17:46 +0630

    Format Code

--- DotNet8.REPRDesignPatternSample.Api/Program.cs
+++ DotNet8.REPRDesignPatternSample.Api/Program.cs
@@ -7,11 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-    opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-}, ServiceLifetime.Transient);
+builder.Services.AddDbContext<AppDbContext>(
+    opt =>
+    {
+        opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+    },
+    ServiceLifetime.Transient
+);
 
 var app = builder.Build();
 
