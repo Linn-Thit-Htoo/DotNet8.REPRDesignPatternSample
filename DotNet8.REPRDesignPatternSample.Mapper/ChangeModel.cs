b725234b9d85fee522a275f239af920701fe4582
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-24 17:18:42 +0630

    Format Code

--- DotNet8.REPRDesignPatternSample.Mapper/ChangeModel.cs
+++ DotNet8.REPRDesignPatternSample.Mapper/ChangeModel.cs
@@ -1,19 +1,18 @@
 ﻿using DotNet8.REPRDesignPatternSample.DbService.AppDbContexts;
 using DotNet8.REPRDesignPatternSample.Models.Features.Blog;
 
-namespace DotNet8.REPRDesignPatternSample.Mapper
+namespace DotNet8.REPRDesignPatternSample.Mapper;
+
+public static class ChangeModel
 {
-    public static class ChangeModel
+    public static BlogModel Change(this Tbl_Blog dataModel)
     {
-        public static BlogModel Change(this Tbl_Blog dataModel)
+        return new BlogModel
         {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
     }
 }
